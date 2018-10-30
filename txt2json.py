4dc2dcf49fa95e02e9ddd34f3df5b7d0ba7d9d83
Arshiaj <joshi.arshia@gmail.com>
2018-10-30 22:59:13 +0530

    Update txt2json.py

--- txt2json.py
+++ txt2json.py
@@ -1,13 +1,8 @@
 import json
 import csv
-
-
-
 x=input("Name?")
-
 f=open(x+"_data.txt")
 data=f.readlines()
-
 with open(x+"_data.csv", 'w',newline='', encoding="utf-8") as csvfile:
     writer = csv.DictWriter(csvfile,fieldnames=["Entities","Labels","Confidence","Count"])
     for i in data:
@@ -21,3 +16,4 @@ with open(x+"_data.csv", 'w',newline='', encoding="utf-8") as csvfile:
             writer.writerow({"Entities":en,"Labels":l,"Confidence":confi,"Count":count})
         
     f.close()
+    //hello Kritika
